65a677b1f5cd5434b76f7562af455abea345e4b2
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-09-02 22:49:57 +0900

    Update and rename .gitignore to main.lua

---
+++ main.lua
@@ -0,0 +1,37 @@
+local GameName = "Nexer Hub - barzil 👏"
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({IntroText = "Nexer Hub 👏", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
+
+OrionLib:MakeNotification({Name = "Warning",Content = "Use at your own risk.",Image = "rbxassetid://7733658504",Time = 5})
+
+local Script = Window:MakeTab({
+	Name = "Script",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Scripts = Script:AddSection({
+	Name = "some cool scripts"
+})
+
+Scripts:AddButton({
+	Name = "giang hub",
+	Callback = function()
+			 loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Slap_Battles/main/Slap_Battles.lua"))()
+	 end
+})
+
+Scripts:AddButton({
+	Name = "fly gui v3",
+	Callback = function()
+			 loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
+	 end
+})
+
+Scripts:AddButton({
+	Name = "btools client side",
+	Callback = function()
+			 loadstring(game:GetObjects("rbxassetid://6695644299")[1].Source)()
+	 end
+})
