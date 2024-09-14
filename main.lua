383065137d42bd15a143b059f14984580e39f160
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-09-14 16:17:18 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -130,7 +130,6 @@ game:GetService("StarterGui"):SetCore("SendNotification",{Title = "nice job",Tex
 BG:AddButton({
 	Name = "Get Fan + Boxer",
 	Callback = function()
-wait(1)
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "script by nexer" ,Duration = 1000, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
 wait(1)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(247.56419372558594, -265.0000305175781, -370.77752685546875)
@@ -146,11 +145,11 @@ wait(0.1)
 game:GetService("ReplicatedStorage").RemoteEvents.KeyBadgeReward:FireServer()
 wait(0.1)
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "nice job",Text = "you got fan :]" ,Duration = 1000, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
-wait(0.1
+wait(1)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(4231.26123046875, 3505.86376953125, 270.4519958496094)
 wait(0.5)
 fireclickdetector(workspace.BoxingGloves.ClickDetector)
-wait(0.5)
+wait(1)
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "nice job",Text = "you got boxing gloves :]" ,Duration = 1000, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
 	 end
 })
